a7e03f695023c19875128efbbe87d2cd932c4f05
Adam Smith <adam@adamsmith.as>
2009-01-26 20:07:50 +0000

    now with moar consumers per server

--- consumers/consumer.py
+++ consumers/consumer.py
@@ -5,11 +5,22 @@ def consumer(f):
   class R(resource.Resource):
     isLeaf = True
   R.render = lambda res, req: f(req)
+  R.tag = f.__name__
   return R()
 
 def serve(res, port):
+  if type(res) is dict:
+    root = resource.Resource()
+    for k,v in res.iteritems():
+      root.putChild(k,v)
+    res = root
+  if type(res) is list:
+    root = resource.Resource()
+    for r in res:
+      root.putChild(r.tag, r)
+    res = root
   reactor.listenTCP(port, server.Site(res))
   reactor.run()
 
-def easy_consume(port):
+def easy_consume(port=80):
   return lambda f: serve(consumer(f),port)
